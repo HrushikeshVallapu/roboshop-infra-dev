602fe355981bf709b27384f2f88e5b454df25496
HrushikeshVallapu <hrushikeshvallapu>
2025-10-19 21:16:23 +0530

    terraform

--- 00-vpc/main.tf
+++ 00-vpc/main.tf
@@ -1,7 +1,7 @@
 module "vpc" {
   source = "git::https://github.com/HrushikeshVallapu/terraform-aws-vpc.git?ref=main"
-  project = "roboshop"
-  environment = "dev"
+  project = var.project
+  environment = var.environment
   public_cidrs_block = ["10.0.1.0/24","10.0.2.0/24"]
   private_cidrs_block = ["10.0.11.0/24","10.0.12.0/24"]
   database_cidrs_block = ["10.0.21.0/24","10.0.22.0/24"]
--- 10-sg/main.tf
+++ 10-sg/main.tf
@@ -2,5 +2,7 @@ module "frontend" {
   source = "git::https://github.com/HrushikeshVallapu/terraform-aws-securitygroup.git?ref=main"
   environment = var.environment
   project = var.project
-  
+  sg_name = var.sg_name
+  description = var.description
+  vpc_id = local.vpc_id
 }
